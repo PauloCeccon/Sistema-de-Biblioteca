a81ec6ca6ada47b22ddd7a4fbccbb29a8cd879d5
Paulo Ceccon <54513864+PauloCeccon@users.noreply.github.com>
2022-05-20 19:38:46 -0300

    Add .

---
+++ sair.php
@@ -0,0 +1,10 @@
+<?php
+$arq1="login.html";
+
+//$_SESSION['usuarioDigitado'] = '';
+//$_SESSION['senhaDigitada'] = '';
+$usuarioDigitado = '';
+$senhaDigitada = '';
+
+header("Location: $arq1");
+?>
